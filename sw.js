7068b24c18fc885a46583a01dba52be59823f859
Seu Nome <seuemail@exemplo.com>
2025-10-30 19:21:18 -0300

    feat: atualiza logo para BarberPro com robô e gera novos ícones PWA 192/512

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker para BarberPro PWA
-const CACHE_NAME = 'barberpro-v2';
+const CACHE_NAME = 'barberpro-v3';
 const urlsToCache = [
   '/barbearia-agendamento25/',
   '/barbearia-agendamento25/index.html',
